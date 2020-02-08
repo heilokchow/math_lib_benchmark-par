28f79d4bd3f88c5ec4e79e1e6e3073e72253d414
heilokchow <u3514104@connect.hku.hk>
2020-02-09 01:31:42 +0800

    parallel version 2.0

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # math_lib_benchmark-par
+
 parallel version of math_lib_benchmark
+
+## Benchmark on multithreading performance
+
+'AVG' is the averaged actual time takes for the whole binary to run for a single replication. Other four categories are the averaged time elapsed for a single replication for each thread.
+
+![Imgur](https://imgur.com/EINtqaH)
--- bench.cpp
+++ bench.cpp
@@ -18,19 +18,29 @@ void check(const int&);
 
 int main(int argc, char** argv)
 {
-    if (argc != 3) {
+    if (argc < 4) {
         std::cout << "No. of input: " << argc << std::endl;
-        puts("./a.out <dim> <nrep>");
+        puts("./a.out <dim> <nrep> [threads]");
         exit(0);
     }
   
     int n = std::stoi(argv[1]);
     int nrep = std::stoi(argv[2]);
+    int num_threads;
+
+    if (argc == 4) {
+        num_threads = std::stoi(argv[3]);
+    }
 
     double sum = 0.0;
-    double t_eigen = 0.0, t_openblas = 0.0; 
+    double t_eigen = 0.0, t_openblas = 0.0;
     double start_eigen, end_eigen, start_openblas, end_openblas;
 
+    std::ofstream out;
+    out.open("result.txt", std::ios_base::app);
+
+    omp_set_num_threads(num_threads);
+    
     // Perform Martix Multiplication
     
     #pragma omp parallel for schedule(dynamic) shared(t_eigen, t_openblas) \
@@ -85,8 +95,8 @@ int main(int argc, char** argv)
         delete[] z;
     }
 
-    printf("\nEigen DGEMM average time: %f\n", t_eigen / nrep);
-    printf("OpenBLAS DGEMM average time: %f\n\n", t_openblas / nrep);
+    out << "DGEMM, Eigen, " << n << ", " << t_eigen / nrep << "\n";
+    out << "DGEMM, OBLAS, " << n << ", " << t_openblas / nrep << "\n";
 
     // Perform Matrix Inversion
     
@@ -114,8 +124,6 @@ int main(int argc, char** argv)
         for (int i = 0; i < n; i++)
             b[i] = normal(generator);
 
-            std::ofstream ot;
-            ot.open("out.txt");
         Eigen::Map<Eigen::MatrixXd, 0, Eigen::Stride<Eigen::Dynamic, 1>>
             Y(&y[0], n, n, Eigen::Stride<Eigen::Dynamic, 1>(n, 1));
         Eigen::Map<Eigen::MatrixXd, 0, Eigen::Stride<Eigen::Dynamic, 1>>
@@ -123,51 +131,20 @@ int main(int argc, char** argv)
         Eigen::Map<Eigen::VectorXd, 0, Eigen::InnerStride<1>> B(&b[0], n);
         Eigen::Map<Eigen::VectorXd, 0, Eigen::InnerStride<1>> X(&x[0], n);
         
-            for (int j = 0; j < n; j++)
-                ot << B(j) << ",";
-            ot << "\n";
-
         YY.noalias() = Y * Y.transpose();
         Eigen::MatrixXd YY_copy(YY);
         X.noalias() = YY * B;
-            for (int j = 0; j < n; j++)
-                ot << x[j] << ",";
-            ot << "\n";
+
         start_eigen = omp_get_wtime();
         B.noalias() = YY_copy.llt().solve(X);
         end_eigen = omp_get_wtime();
 
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++)
-                    ot << yy[i*n+j] << ",";
-                ot << "\n";
-            }
-            
-            for (int j = 0; j < n; j++)
-                ot << x[j] << ",";
-            ot << "\n";
         start_openblas = omp_get_wtime();
         LAPACKE_dgesv(LAPACK_ROW_MAJOR, n, 1, yy, n, ipiv, x, 1);
         end_openblas = omp_get_wtime();
 
         for (int i = 0; i < n; i++)
             sum += abs(x[i] - b[i]);
-        
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++)
-                    ot << YY_copy(i, j) << ",";
-                ot << "\n";
-            }
-            
-            for (int j = 0; j < n; j++)
-                ot << x[j] << ",";
-            ot << "\n";
-
-            for (int j = 0; j < n; j++)
-                ot << B(j) << ",";
-            ot << "\n";
-
-            ot.close();
 
         int id = omp_get_thread_num();
         printf("ID: %d, Error: %f #Linear Solver\n", id, sum);
@@ -185,101 +162,9 @@ int main(int argc, char** argv)
         delete[] ipiv;
     }
 
-    printf("\nEigen DGESV average time: %f\n", t_eigen / nrep);
-    printf("OpenBLAS DEGSV average time: %f\n\n", t_openblas / nrep);
-
-    check(n);
+    out << "DGESV, Eigen, " << n << ", " << t_eigen / nrep << "\n";
+    out << "DGESV, OBLAS, " << n << ", " << t_openblas / nrep << "\n";
+    out.close();
 
     return 0;
-}
-
-void check(const int& n) {
-    double* x = new double[n * n];
-    double* y = new double[n * n];
-    double* z = new double[n * n];
-    double* y1 = new double[n * n];
-    double* y2 = new double[n * n];
-    double* y3 = new double[n * n];
-    double* a = new double[n];
-    double* b = new double[n];
-    int* ipiv = new int[n];
-    int p = 0, q = 0;
-    std::random_device device;
-    std::mt19937 generator(device());
-    std::normal_distribution<double> normal(0.0, 1.0);
-    double c1 = 0.0, c2 = 0.0, c3 = 0.0;
-    for (size_t i = 0; i < n; i++) {
-        for (size_t j = 0; j < n; j++) {
-            p = i * n + j;
-            q = j * n + i;
-            y1[p] = normal(generator);
-            y2[q] = y1[p];
-            z[p] = normal(generator);
-            z[q] = z[p];
-            x[p] = 0.0;
-            x[q] = 0.0;
-        }
-        a[i] = normal(generator);
-        b[i] = 0.0;
-    }
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1.0, y1, n, y2, n, 0.0, y, n);
-
-    Eigen::MatrixXd X(n, n), Y(n, n), Z(n, n);
-    Eigen::VectorXd A(n), B(n);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            p = i * n + j;
-            Y(i, j) = y[p];
-            Z(i, j) = z[p];
-            X(i, j) = 0.0;
-        }
-        A(i) = a[i];
-        B(i) = b[i];
-    }
-
-    // DGEMM
-    X.noalias() = Y * Z;
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, n, n, 1.0, y, n, z, n, 0.0, x, n);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            c1 = c1 + abs(x[i * n + j] - X(i, j));
-        }
-    }
-
-    // DGESV
-    B = Y * A;
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, 1, n, 1.0, y, n, a, 1, 0.0, b, 1);
-    
-    A = Y.llt().solve(B);
-    for (size_t i = 0; i < n; i++) {
-        for (size_t j = 0; j < n; j++) {
-            y3[i * n + j] = y[i * n + j];
-        }
-    }
-    LAPACKE_dgesv(LAPACK_ROW_MAJOR, n, 1, y, n, ipiv, b, 1);
-
-    for (int i = 0; i < n; i++) {
-        c2 = c2 + abs(b[i] - A(i));
-    }
-
-    //DPOTRF
-    Eigen::MatrixXd T = Y.llt().matrixL();
-    LAPACKE_dpotrf(LAPACK_ROW_MAJOR, 'L', n, y3, n);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < (i + 1); j++) {
-           c3 = c3 + abs(T(i, j) - y3[i * n + j]);
-        }
-    }
-    
-    std::cout << c1 << "," << c2 << "," << c3 << '\n';
-
-    delete[] x;
-    delete[] y;
-    delete[] z;
-    delete[] y1;
-    delete[] y2;
-    delete[] y3;
-    delete[] a;
-    delete[] b;
-    delete[] ipiv;
 }
